4890156b84b183039ef3285efc63e44be88cfb2e
lcj0304 <lcj0304@qq.com>
2025-04-15 22:42:55 +0800

    函数测试

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+    let result = add(5, 3);
+    println!("The sum of 5 and 3 is: {}", result);
+}
+
+
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
